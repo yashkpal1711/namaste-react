b746b5fed4c6c423f33b17069ec2905cdf222e99
yashkpal1711 <69109847+yashkpal1711@users.noreply.github.com>
2023-09-28 14:48:55 +0530

    updated restaurant cards

--- App.js
+++ App.js
@@ -38,13 +38,16 @@ const UserIcon = () => (
   </a>
 );
 
-const RestaurantCard = ({name,image,stars}) => (
+const RestaurantCard = ({ name, image, stars }) => (
   <div className="restaurantCard">
     <img
       alt="food picture"
-      src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"+ image}
+      src={
+        "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
+        image
+      }
     />
-        <h2>{name}</h2>
+    <h2>{name}</h2>
     <h3>{stars}</h3>
   </div>
 );
@@ -58,38 +61,32 @@ const RestaurantCardContainer = () => (
 const data = [
   {
     name: "Om Sweets & Snacks",
-    image:
-      "l7llnbfv3rhnp8cgsepo",
+    image: "l7llnbfv3rhnp8cgsepo",
     stars: 4.4,
   },
   {
     name: "Mac D",
-    image:
-      "85ccae4e3576f9330af102c46ca85395",
+    image: "85ccae4e3576f9330af102c46ca85395",
     stars: 1.4,
   },
   {
     name: "KFC",
-    image:
-      "56c9ab92bd79745fd152a30fa2525426",
+    image: "56c9ab92bd79745fd152a30fa2525426",
     stars: 3.4,
   },
   {
     name: "burger King",
-    image:
-      "85ccae4e3576f9330af102c46ca85395",
+    image: "85ccae4e3576f9330af102c46ca85395",
     stars: 2.4,
   },
   {
     name: "biryani blues",
-    image:
-      "85ccae4e3576f9330af102c46ca85395",
+    image: "85ccae4e3576f9330af102c46ca85395",
     stars: 4.1,
   },
   {
     name: "kullad Pizza",
-    image:
-      "l7llnbfv3rhnp8cgsepo",
+    image: "l7llnbfv3rhnp8cgsepo",
     stars: 5,
   },
 ];
@@ -112,6 +109,7 @@ const App = () => (
     <RestaurantCardContainer />
   </>
 );
+
 
 const root = ReactDOM.createRoot(document.getElementById("container"));
 root.render(<App />);
