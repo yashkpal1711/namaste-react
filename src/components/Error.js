e88ea9adf20d3a22ba4c898321076fea12af029e
yashkpal1711 <69109847+yashkpal1711@users.noreply.github.com>
2023-10-13 13:56:49 +0530

    Added a navigation bar and login button to the header.

---
+++ src/components/Error.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { useRouteError } from "react-router-dom";
+import { data } from './../Config';
+
+const Error = () => {
+  const err = useRouteError();
+  return (
+    <>
+      <div>
+        Opps something went wrong,
+        <br /> Don't worry the developer is working hard towards fixing this bug
+        😅
+      </div>
+      <h3>{(err.data)}</h3>
+    </>
+  );
+};
+
+export default Error;
