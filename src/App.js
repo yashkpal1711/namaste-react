98ac6c30cb14e72e42b7c7b12723f8177c6bec1f
yashkpal1711 <69109847+yashkpal1711@users.noreply.github.com>
2023-10-13 13:55:27 +0530

    Added navigation bar and login button to the header, and made the search bar wider.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Error from "./components/Error";
+import AboutUs from "./components/AboutUs";
 
 const App = () => (
   <>
@@ -10,5 +13,17 @@ const App = () => (
   </>
 );
 
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/about",
+    element: <AboutUs />,
+  },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("container"));
-root.render(<App />);
+root.render(<RouterProvider router={appRouter} />);
